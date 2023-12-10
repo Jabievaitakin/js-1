c4c60107c949f43591e13ad44643c1aa9f01f52e
Jabievaitakin <aitakin.jabi@gmail.com>
2023-12-10 16:43:56 +0400

    task is done

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let birthYear = 2005; 
+let futureYear =2025;
+
+
+console.log( "1ci texmini olunan yasim:martdan sonra " + (futureYear -birthYear));
+console.log("2ci texmini olunan yasim:martdan evvel " + ((futureYear-birthYear)-1))
